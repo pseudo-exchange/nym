48b7b5312703f96650a9f6209758728eb9f7734a
trevorjtclarke <trevorjtclarke@gmail.com>
2021-03-24 22:06:23 -0700

    working through use cases

---
+++ account_manager/src/README.md
@@ -0,0 +1,66 @@
+# Account Manager
+
+A tiny contract to manage account ownership and transfer functionality, by utilizing an escrow witness as the coordinator
+
+### Transfer Flows
+
+#### General Workflow
+
+1. Initialization
+2. IF too many keys -- Remove Keys until only escrow
+3. IF owner changes mind -- Revert Ownership
+4. Transfer ownership
+
+#### Initialization
+
+This happens upon contract deploy from escrow contract. This requires the user to grant escrow public key access to their account. All precautions must be taken before initialization can proceed.
+
+1. User grants access to escrow, adding full access key to the account being transfered
+2. Escrow contract deploys an Account Manager (AM) contract, the code in this folder
+3. Escrow calls the function "new" on the newly deployed AM contract, which executes the following logic:
+  3A. Make sure the initialization is valid
+  3B. Transfer any/all balance on this account (No ability to know any sub account balances)
+  3C. Change access keys to hand over full ownership to escrow
+  3D. Finish by assigning state, for future contract calls
+
+#### Removing Keys
+
+In the event that escrow system determines there are more access keys than are needed, escrow will use this function to fully clear any remaining keys. This function will be executed directly at the consent of the user, as they are in fact paying to have this done. It is necessary to maintain the risk by forcing the user to pay in this case.
+
+1. User gets a warning in a web UI showing there are more full access keys to remove
+2. User signs a transaction to remove access key, which isnt allowed to remove escrow, only all other access keys.
+
+#### Reverting Ownership
+
+At some point, an owner will decide they no longer want to leave an account in escrow. They can utilize this function to revert the ownership to the original access keys. This does not however, allow the user to revert to any new access keys, and if the originals are lost, a transfer via escrow MUST be made.
+
+1. Check that only escrow can call, and txn is valid
+2. Add full access key back to known original owner
+3. Remove escrow full access key
+
+#### Transfer of Ownership
+
+The escrow contract will manage the movement from escrow ownership into the new access keys. This is done by allowing the escrow to make judgements about whether an account is available and ready to make a transfer. Such logic and caveats can be seen in the escrow folder.
+
+1. Check that access & txn are valid
+2. Add new full access key, the new owner
+3. Remove escrow full access key
+
+### Commands & Usage
+
+Requires [near cli]()
+
+```bash
+# Init
+near call 
+
+# transfer ownership
+near call _account_here_ transfer_ownership '{"new_owner_pk": "ed25591:PK_HERE"}' --accountId youraccount.testnet
+
+# revert ownership
+near call _account_here_ revert_ownership '{}' --accountId youraccount.testnet
+
+# remove key
+near call _account_here_ remove_key '{"remove_key": "ed25591:PK_HERE"}' --accountId youraccount.testnet
+
+```
--- account_manager/src/lib.rs
+++ account_manager/src/lib.rs
@@ -1,5 +1,5 @@
 use near_sdk::borsh::{self, BorshDeserialize, BorshSerialize};
-use near_sdk::json_types::{Base58PublicKey};
+use near_sdk::json_types::{ ValidAccountId, Base58PublicKey };
 use near_sdk::{
     env, Promise, near_bindgen, setup_alloc, PanicOnDefault, PublicKey
 };
@@ -9,6 +9,7 @@ setup_alloc!();
 #[near_bindgen]
 #[derive(BorshDeserialize, BorshSerialize, PanicOnDefault)]
 pub struct AccountManager {
+    escrow_account_id: ValidAccountId,
     owner_pk: PublicKey,
     pk: PublicKey
 }
@@ -19,29 +20,63 @@ pub struct AccountManager {
 // Has a function to change owner, callable ONLY by escrow.nym.near
 #[near_bindgen]
 impl AccountManager {
+    /// Upon deploy, contract initializes with only escrow account owning this account
+    /// All funds and previous ownership MUST be removed before escrow can validate
+    /// the account is available for any other ownership transfers
     #[init]
-    pub fn new(escrow_pk: Base58PublicKey, original_owner_pk: Base58PublicKey) -> Self {
+    pub fn new(escrow_pk: Base58PublicKey, escrow_account_id: ValidAccountId, original_owner_pk: Base58PublicKey) -> Self {
+        assert_ne!(env::signer_account_id(), env::current_account_id(), "Cannot sign against current account");
+        assert_ne!(&escrow_pk, &original_owner_pk, "Cannot use same keys");
+
+        // Transfer out remaining value?
+        let p1 = Promise::new(env::signer_account_id())
+            .transfer(env::account_balance());
+
+        let p2 = Promise::new(env::current_account_id())
+            .add_full_access_key(escrow_pk.clone().into())
+            .delete_key(original_owner_pk.clone().into());
+
+        p1.then(p2);
+
         AccountManager {
+            escrow_account_id,
             owner_pk: original_owner_pk.into(),
             pk: escrow_pk.into()
         }
     }
 
+    /// Allows original owner to get access back to this account
+    /// This function is only available to the original owner
     pub fn revert_ownership(&mut self) -> Promise {
-        // TODO: Add asserts
+        assert_eq!(env::predecessor_account_id(), self.escrow_account_id.to_string(), "Unauthorized access, escrow only");
+        assert_eq!(self.owner_pk, env::signer_account_pk(), "Invalid Owner");
+
         Promise::new(env::current_account_id())
-            .add_full_access_key(self.owner_pk)
-            .delete_key(self.pk)
+            .add_full_access_key(self.owner_pk.clone())
+            .delete_key(self.pk.clone())
     }
 
+    /// Called only by the escrow contract, this allows the escrow to fully capture
+    /// the account, only having a single key for ownership
+    pub fn remove_key(&mut self, remove_key: Base58PublicKey) -> Promise {
+        assert_eq!(env::predecessor_account_id(), self.escrow_account_id.to_string(), "Unauthorized access, escrow only");
+        assert_ne!(remove_key.to_owned(), self.pk, "Cannot remove escrow");
+
+        Promise::new(env::current_account_id())
+            .delete_key(remove_key.into())
+    }
+
+    // TODO: Deploy a new contract over this one to complete the transfer of ownership?
     pub fn transfer_ownership(&mut self, new_owner_pk: Base58PublicKey) -> Promise {
-        // TODO: Add asserts
+        assert_eq!(env::predecessor_account_id(), self.escrow_account_id.to_string(), "Unauthorized access, escrow only");
+
         Promise::new(env::current_account_id())
             .add_full_access_key(new_owner_pk.into())
-            .delete_key(self.pk)
+            .delete_key(self.pk.clone())
     }
 }
 
+// TODO: Test - ownership transfer is successful, reverting can only happen by original owner
 #[cfg(all(test, not(target_arch = "wasm32")))]
 mod tests {
     use near_sdk::test_utils::{accounts, VMContextBuilder};
