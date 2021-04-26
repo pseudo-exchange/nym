52302cb5b126dd2c90bb9b231588407ae3b0415d
trevorjtclarke <trevorjtclarke@gmail.com>
2021-04-25 19:11:19 -0700

    finished deed tests

--- escrow/src/lib.rs
+++ escrow/src/lib.rs
@@ -2,7 +2,7 @@ use near_sdk::{
     ext_contract,
     near_bindgen,
     borsh::{self, BorshDeserialize, BorshSerialize},
-    // serde_json::{json},
+    serde_json::{json},
     collections::{ LookupMap },
     json_types::{ ValidAccountId, Base58PublicKey },
     AccountId,
@@ -12,10 +12,36 @@ use near_sdk::{
     PublicKey,
     PanicOnDefault,
 };
+use std::convert::TryFrom;
 
 near_sdk::setup_alloc!();
 
-const ESCROW_STORAGE_KEY: [u8; 1] = [0];
+const ESCROW_STORAGE_TLA_KEY: [u8; 1] = [0];
+const ESCROW_STORAGE_ACCOUNT_KEY: [u8; 1] = [1];
+
+// TODO: Finalize amounts needed!
+// Ⓝa Ⓝa Ⓝa Ⓝa Ⓝa Ⓝa Ⓝa Ⓝa - Batmannnnnnnn
+pub const ONE_NEAR: u128 = 1_000_000_000_000_000_000_000_000;
+const NEW_ACCOUNT_STORAGE_AMOUNT: u128 = ONE_NEAR * 3;
+const MAX_GAS_FEE: u64 = 300_000_000_000_000;
+const NEW_ACCOUNT_GAS_FEE: u64 = MAX_GAS_FEE / 3;
+const DELETE_ACCOUNT_GAS_FEE: u64 = MAX_GAS_FEE / 3;
+const REGISTER_CALLBACK_GAS_FEE: u64 = MAX_GAS_FEE / 2;
+// const REGISTRAR: &str = "registrar";
+
+#[ext_contract(ext_self)]
+trait ExtSelf {
+    fn finalize_register(&mut self, title: ValidAccountId, underwriter: ValidAccountId) -> Promise;
+}
+
+#[ext_contract]
+trait ExtAccountFactory {
+    fn create_account(
+        &mut self,
+        new_account_id: AccountId,
+        new_public_key: Base58PublicKey,
+    ) -> Promise;
+}
 
 #[ext_contract]
 pub trait ExtDeed {
@@ -25,47 +51,113 @@ pub trait ExtDeed {
     fn transfer_ownership(&mut self, new_owner_pk: PublicKey) -> Promise;
 }
 
+#[ext_contract]
+pub trait ExtTransferOwner {
+    fn delete_self(&self) -> Promise;
+}
+
 #[near_bindgen]
 #[derive(BorshDeserialize, BorshSerialize, PanicOnDefault)]
 pub struct Escrow {
-    // THIS account id, most cases: escrow_nym.near
+    /// THIS account id, most cases: escrow_nym.near
     id: AccountId,
+    pk: PublicKey,
+
+    /// The account that can create base accounts (in testnet, factory_id: "testnet", mainnet factory_id: "near")
+    factory_id: AccountId,
+
+    /// The account that handles all logic for auctions, bids & other actions
+    auction_id: AccountId,
 
     // TODO: Owner -- auction house?
     owner_id: Option<AccountId>,
 
     // keeps track of the escrowed accounts
+    tlas: LookupMap<AccountId, AccountId>,
     accounts: LookupMap<AccountId, AccountId>
 }
 
-// Contract keeps track of accounts in escrow
-// is the only account that can execute functions on escrowed account
-// should not be owned by anyone -- NO ACCESS KEYS!
+/// Escrow
+/// Contract keeps track of accounts in escrow
+/// is the only account that can execute functions on escrowed account
+/// should not be owned by anyone -- NO ACCESS KEYS!
 #[near_bindgen]
 impl Escrow {
     #[init(ignore_state)]
-    pub fn new() -> Self {
+    pub fn new(
+        factory_id: AccountId,
+        auction_id: AccountId,
+        pk: PublicKey
+    ) -> Self {
         Escrow {
             id: env::current_account_id(),
-            accounts: LookupMap::new(ESCROW_STORAGE_KEY.to_vec()),
+            pk,
+            factory_id,
+            auction_id,
+            tlas: LookupMap::new(ESCROW_STORAGE_TLA_KEY.to_vec()),
+            accounts: LookupMap::new(ESCROW_STORAGE_ACCOUNT_KEY.to_vec()),
             owner_id: None,
         }
     }
 
+    // TODO: finish
+    /// Callback of the register function
+    /// Responsible for creating the account again, bonding to escrow holdings
+    /// Deploys a contract to the new account, such that
+    pub fn finalize_register(&mut self, title: ValidAccountId, underwriter: ValidAccountId) -> Promise {
+        // add to registry
+        self.accounts.insert(&title.to_string(), &underwriter.to_string());
+        log!("New deed: {}", &title);
+
+        // now that account has been deleted, create again!
+        ext_account_factory::create_account(
+            title.clone().to_string(),
+            Base58PublicKey::try_from(env::signer_account_pk()).unwrap(),
+            &self.factory_id,
+            NEW_ACCOUNT_STORAGE_AMOUNT,
+            NEW_ACCOUNT_GAS_FEE
+        )
+        .then(
+            // deploy deed contract, to allow final management
+            Promise::new(underwriter.clone().to_string())
+                .deploy_contract(
+                    include_bytes!("../../res/deed.wasm").to_vec(),
+                )
+                // TODO: 
+                // .then(ext_deed::new())
+        )
+    }
+
+    // TODO:
     /// Responsible for bonding an account to a deed contract, where
     /// escrow is the sole owner, and can only transfer ownership upon
     /// close of title
     // NOTE: Currenly only possible if this escrow account has a public key with full access to account, otherwise deploy is not possible.
-    pub fn register(&mut self, underwriter: ValidAccountId) {
-        let title = env::signer_account_id();
+    #[payable]
+    pub fn register(&mut self, underwriter: ValidAccountId) -> Promise {
+        let title = ValidAccountId::try_from(env::signer_account_id()).unwrap();
         // Make sure this account isnt already in escrow
         assert_ne!(self.accounts.contains_key(&title.to_string()), true, "Account already in escrow");
 
-        // add to registry
-        self.accounts.insert(&title.to_string(), &underwriter.to_string());
-        log!("New deed: {}", &title);
+        Promise::new(title.clone().to_string())
+            .function_call(
+                b"delete_self".to_vec(),
+                json!({}).to_string().as_bytes().to_vec(),
+                0,
+                DELETE_ACCOUNT_GAS_FEE
+            )
+            .then(
+                ext_self::finalize_register(
+                    title,
+                    underwriter,
+                    &env::current_account_id(),
+                    0,
+                    REGISTER_CALLBACK_GAS_FEE
+                )
+            )
     }
 
+    // TODO: Finish gas needs
     /// Allows an owner to cancel a deed, given appropriate parameters
     pub fn revert_title(&mut self, title: AccountId) -> Promise {
         self.is_in_escrow(title.clone());
@@ -82,6 +174,7 @@ impl Escrow {
         )
     }
 
+    // TODO: Finish gas needs
     /// Removes any excess public keys on an account, to ensure full
     /// Account transfer can happen trustlessly
     pub fn encumbrance(&self, title: AccountId, key: Base58PublicKey) -> Promise {
@@ -97,11 +190,14 @@ impl Escrow {
         )
     }
 
+    // TODO: Finish gas needs
     /// The full realization of an escrow deed, where the account is
     /// transferred to the new owner
     pub fn close_escrow(&mut self, title: AccountId, new_key: PublicKey) -> Promise {
         self.is_in_escrow(title.clone());
-        // TODO: Can only be called by auction house
+        // TODO: 
+        // Can only be called by auction house
+        assert_eq!(self.auction_id, env::predecessor_account_id(), "Must be called only by auction");
 
         // Remove from registry
         self.accounts.remove(&title.to_string());
